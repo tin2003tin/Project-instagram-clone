c1cb7155dd4872b2588d33405eeb0a691733361c
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2023-12-03 19:09:08 +0700

    Update usePreviewImg.js

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -2,32 +2,32 @@ import { useState } from "react";
 import useShowToast from "./useShowToast";
 
 const usePreviewImg = () => {
-	const [selectedFile, setSelectedFile] = useState(null);
-	const showToast = useShowToast();
-	const maxFileSizeInBytes = 2 * 1024 * 1024; // 2MB
+  const [selectedFile, setSelectedFile] = useState(null);
+  const showToast = useShowToast();
+  const maxFileSizeInBytes = 3 * 1024 * 1024; // 3MB
 
-	const handleImageChange = (e) => {
-		const file = e.target.files[0];
-		if (file && file.type.startsWith("image/")) {
-			if (file.size > maxFileSizeInBytes) {
-				showToast("Error", "File size must be less than 2MB", "error");
-				setSelectedFile(null);
-				return;
-			}
-			const reader = new FileReader();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file && file.type.startsWith("image/")) {
+      if (file.size > maxFileSizeInBytes) {
+        showToast("Error", "File size must be less than 3MB", "error");
+        setSelectedFile(null);
+        return;
+      }
+      const reader = new FileReader();
 
-			reader.onloadend = () => {
-				setSelectedFile(reader.result);
-			};
+      reader.onloadend = () => {
+        setSelectedFile(reader.result);
+      };
 
-			reader.readAsDataURL(file);
-		} else {
-			showToast("Error", "Please select an image file", "error");
-			setSelectedFile(null);
-		}
-	};
+      reader.readAsDataURL(file);
+    } else {
+      showToast("Error", "Please select an image file", "error");
+      setSelectedFile(null);
+    }
+  };
 
-	return { selectedFile, handleImageChange, setSelectedFile };
+  return { selectedFile, handleImageChange, setSelectedFile };
 };
 
 export default usePreviewImg;
