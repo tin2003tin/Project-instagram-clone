3fe98b60995474d844272acb51f5a4eba7ba1ee6
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2023-12-03 18:49:59 +0700

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,17 +4,17 @@ import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
 
 const HomePage = () => {
 	return (
-		<Container maxW={"container.lg"}>
-			<Flex gap={20}>
-				<Box flex={2} py={10}>
-					<FeedPosts />
-				</Box>
-				<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
-					<SuggestedUsers />
-				</Box>
-			</Flex>
-		</Container>
-	);
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10}>
+          <FeedPosts />
+        </Box>
+        <Box flex={3} mr={20} maxW={"300px"} overflowX="auto">
+          <SuggestedUsers />
+        </Box>
+      </Flex>
+    </Container>
+  );
 };
 
 export default HomePage;
