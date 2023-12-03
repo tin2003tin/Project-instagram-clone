fedd60e37b5f515d34971203ae1b9d4847bc6e16
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2023-12-03 18:26:53 +0700

    fix-v1

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -63,116 +63,141 @@ const ProfilePost = ({ post }) => {
 	};
 
 	return (
-		<>
-			<GridItem
-				cursor={"pointer"}
-				borderRadius={4}
-				overflow={"hidden"}
-				border={"1px solid"}
-				borderColor={"whiteAlpha.300"}
-				position={"relative"}
-				aspectRatio={1 / 1}
-				onClick={onOpen}
-			>
-				<Flex
-					opacity={0}
-					_hover={{ opacity: 1 }}
-					position={"absolute"}
-					top={0}
-					left={0}
-					right={0}
-					bottom={0}
-					bg={"blackAlpha.700"}
-					transition={"all 0.3s ease"}
-					zIndex={1}
-					justifyContent={"center"}
-				>
-					<Flex alignItems={"center"} justifyContent={"center"} gap={50}>
-						<Flex>
-							<AiFillHeart size={20} />
-							<Text fontWeight={"bold"} ml={2}>
-								{post.likes.length}
-							</Text>
-						</Flex>
+    <>
+      <GridItem
+        cursor={"pointer"}
+        borderRadius={4}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
+      >
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={"blackAlpha.700"}
+          transition={"all 0.3s ease"}
+          zIndex={1}
+          justifyContent={"center"}
+        >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+              <AiFillHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                {post.likes.length}
+              </Text>
+            </Flex>
 
-						<Flex>
-							<FaComment size={20} />
-							<Text fontWeight={"bold"} ml={2}>
-								{post.comments.length}
-							</Text>
-						</Flex>
-					</Flex>
-				</Flex>
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                {post.comments.length}
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
 
-				<Image src={post.imageURL} alt='profile post' w={"100%"} h={"100%"} objectFit={"cover"} />
-			</GridItem>
+        <Image
+          src={post.imageURL}
+          alt="profile post"
+          w={"100%"}
+          h={"100%"}
+          objectFit={"cover"}
+        />
+      </GridItem>
 
-			<Modal isOpen={isOpen} onClose={onClose} isCentered={true} size={{ base: "3xl", md: "5xl" }}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalCloseButton />
-					<ModalBody bg={"black"} pb={5}>
-						<Flex
-							gap='4'
-							w={{ base: "90%", sm: "70%", md: "full" }}
-							mx={"auto"}
-							maxH={"90vh"}
-							minH={"50vh"}
-						>
-							<Flex
-								borderRadius={4}
-								overflow={"hidden"}
-								border={"1px solid"}
-								borderColor={"whiteAlpha.300"}
-								flex={1.5}
-								justifyContent={"center"}
-								alignItems={"center"}
-							>
-								<Image src={post.imageURL} alt='profile post' />
-							</Flex>
-							<Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "flex" }}>
-								<Flex alignItems={"center"} justifyContent={"space-between"}>
-									<Flex alignItems={"center"} gap={4}>
-										<Avatar src={userProfile.profilePicURL} size={"sm"} name='As a Programmer' />
-										<Text fontWeight={"bold"} fontSize={12}>
-											{userProfile.username}
-										</Text>
-									</Flex>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered={true}
+        size={{ base: "3xl", md: "5xl" }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody bg={"black"} pb={5}>
+            <Flex
+              gap="4"
+              w={{ base: "90%", sm: "70%", md: "full" }}
+              mx={"auto"}
+              maxH={"90vh"}
+              minH={"50vh"}
+              flexDirection={{ base: "row", sm: "column" }}
+            >
+              <Flex
+                borderRadius={4}
+                overflow={"hidden"}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+                flex={1.5}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Image src={post.imageURL} alt="profile post" />
+              </Flex>
+              <Flex
+                flex={1}
+                flexDir={"column"}
+                px={10}
+              >
+                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                  <Flex alignItems={"center"} gap={4}>
+                    <Avatar
+                      src={userProfile.profilePicURL}
+                      size={"sm"}
+                      name="As a Programmer"
+                    />
+                    <Text fontWeight={"bold"} fontSize={12}>
+                      {userProfile.username}
+                    </Text>
+                  </Flex>
 
-									{authUser?.uid === userProfile.uid && (
-										<Button
-											size={"sm"}
-											bg={"transparent"}
-											_hover={{ bg: "whiteAlpha.300", color: "red.600" }}
-											borderRadius={4}
-											p={1}
-											onClick={handleDeletePost}
-											isLoading={isDeleting}
-										>
-											<MdDelete size={20} cursor='pointer' />
-										</Button>
-									)}
-								</Flex>
-								<Divider my={4} bg={"gray.500"} />
+                  {authUser?.uid === userProfile.uid && (
+                    <Button
+                      size={"sm"}
+                      bg={"transparent"}
+                      _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                      borderRadius={4}
+                      p={1}
+                      onClick={handleDeletePost}
+                      isLoading={isDeleting}
+                    >
+                      <MdDelete size={20} cursor="pointer" />
+                    </Button>
+                  )}
+                </Flex>
+                <Divider my={4} bg={"gray.500"} />
 
-								<VStack w='full' alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
-									{/* CAPTION */}
-									{post.caption && <Caption post={post} />}
-									{/* COMMENTS */}
-									{post.comments.map((comment) => (
-										<Comment key={comment.id} comment={comment} />
-									))}
-								</VStack>
-								<Divider my={4} bg={"gray.8000"} />
+                <VStack
+                  w="full"
+                  alignItems={"start"}
+                  maxH={"350px"}
+                  overflowY={"auto"}
+                >
+                  {/* CAPTION */}
+                  {post.caption && <Caption post={post} />}
+                  {/* COMMENTS */}
+                  {post.comments.map((comment) => (
+                    <Comment key={comment.id} comment={comment} />
+                  ))}
+                </VStack>
+                <Divider my={4} bg={"gray.8000"} />
 
-								<PostFooter isProfilePage={true} post={post} />
-							</Flex>
-						</Flex>
-					</ModalBody>
-				</ModalContent>
-			</Modal>
-		</>
-	);
+                <PostFooter isProfilePage={true} post={post} />
+              </Flex>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default ProfilePost;
--- src/store/postStore.js
+++ src/store/postStore.js
@@ -1,22 +1,37 @@
 import { create } from "zustand";
 
 const usePostStore = create((set) => ({
-	posts: [],
-	createPost: (post) => set((state) => ({ posts: [post, ...state.posts] })),
-	deletePost: (id) => set((state) => ({ posts: state.posts.filter((post) => post.id !== id) })),
-	setPosts: (posts) => set({ posts }),
-	addComment: (postId, comment) =>
-		set((state) => ({
-			posts: state.posts.map((post) => {
-				if (post.id === postId) {
-					return {
-						...post,
-						comments: [...post.comments, comment],
-					};
-				}
-				return post;
-			}),
-		})),
+  posts: [],
+  createPost: (post) => set((state) => ({ posts: [post, ...state.posts] })),
+  deletePost: (id) =>
+    set((state) => ({ posts: state.posts.filter((post) => post.id !== id) })),
+  setPosts: (posts) => set({ posts }),
+  setLike: (postId, uid) =>
+    set((state) => ({
+      posts: state.posts.map((post) => {
+        if (post.id === postId) {
+          return {
+            ...post,
+            likes: post.likes.includes(uid)
+              ? post.likes.filter((like) => like !== uid)
+              : [...post.likes, uid],
+          };
+        }
+        return post;
+      }),
+    })),
+  addComment: (postId, comment) =>
+    set((state) => ({
+      posts: state.posts.map((post) => {
+        if (post.id === postId) {
+          return {
+            ...post,
+            comments: [...post.comments, comment],
+          };
+        }
+        return post;
+      }),
+    })),
 }));
 
 export default usePostStore;
